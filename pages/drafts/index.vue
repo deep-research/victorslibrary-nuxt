d80a4f49a03868af68797b095be3bcebf2f39072
Deep Research <fisheve@mx.lakeforest.edu>
2021-08-30 15:23:10 -0500

    Drafts Redirect Working

---
+++ pages/drafts/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <error></error>
+  </div>
+</template>
+
+    <script>
+    import error from  '../../layouts/error.vue'
+
+    export default {
+      async asyncData({ error }) {
+        error({ statusCode: 404, message: 'Page not found' })
+      },
+      components: {
+        error
+      }
+    }
+    </script>
