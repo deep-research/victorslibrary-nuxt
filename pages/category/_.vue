6cbc5a09e6ca44b123fe775160e9a2bf9d254177
Deep Research <fisheve@mx.lakeforest.edu>
2021-08-30 23:44:06 -0500

    Both Types of Routing Together

--- pages/category/_.vue
+++ pages/category/_.vue
@@ -9,9 +9,9 @@
 <script lang="ts">
 export default {
   async asyncData ({ $content, app, params, error }: {$content: any, app: any, params: any, error: any }) {
-    const path = `/articles/${params.pathMatch || 'index'}`
+    const path = `/category/${params.pathMatch || 'index'}`
     console.log(path)
-    const [article] = await $content("articles", { deep: true }).where({ path }).fetch()
+    const [article] = await $content("category", { deep: true }).where({ path }).fetch()
 
     if (!article) {
       return error({ statusCode: 404, message: 'Article not found' })
--- pages/category/index.vue
+++ pages/category/index.vue
@@ -1,23 +1,20 @@
 <template>
   <article>
-    <h1>All Pages</h1>
+    <h1>All Categories</h1>
     <h1 v-if="dev">Currently in development</h1>
     <h1 v-else>Currently in production</h1>
 
     <div v-for="article in articles" :key="article.id">
-      <h1><nuxt-link :to="article.slug">{{ article.title }}</nuxt-link></h1>
+      <h1><nuxt-link :to="article.path">{{ article.title }}</nuxt-link></h1>
       <div v-if="article.lyrics" v-html="$md.render(String(article.lyrics))"></div>
       <nuxt-content :document="article" />
     </div>
-
-    <br />
-    <h1>Category</h1>
-    <h1><nuxt-link to="/category">Category Search</nuxt-link></h1>
   </article>
 </template>
 
 <script lang="ts">
-  import Config from "../nuxt.config.js"
+  import Config from "../../nuxt.config.js"
+
   export default {
     data () {
       return {
@@ -25,10 +22,12 @@
       }
     },
     async asyncData ({ $content, error }: {$content: any, error: any })  {
-      const articles = await $content('/articles', { deep: true }).fetch()
+      const articles = await $content('/category', { deep: true }).fetch()
+
       if (!articles) {
         return error({ statusCode: 404, message: 'Article not found' })
       }
+
       return {
         articles
       }
