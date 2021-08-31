1c2a87ae71e88d13b0306609bfd4f30fe6a78601
Deep Research <fisheve@mx.lakeforest.edu>
2021-08-30 22:10:31 -0500

    All nested articles in a folder display on a single route

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
 export default {
-  async asyncData ({ $content, app, params, error }: { $content: any, app: any, params: any, error: any }) {
+  async asyncData ({ $content, params, error }: { $content: any, app: any, params: any, error: any }) {
     const [article] = await $content("articles", { deep: true }).where({slug: params.slug}).fetch()
 
     if (!article) {
