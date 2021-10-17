05ee8cd72995aa96b4826eec147b1e32c191f947
Deep Research <fisheve@mx.lakeforest.edu>
2021-10-17 16:03:54 -0500

    Update post-info.vue

--- components/post-info.vue
+++ components/post-info.vue
@@ -63,8 +63,7 @@ export default Vue.extend({
   async fetch() {
     let article: any = await this.$content('/', { deep: true }).where({title: this.title}).fetch()
     this.article = article[0]
-  },
-  fetchKey: 'post-info'
+  }
 })
 </script>
 
