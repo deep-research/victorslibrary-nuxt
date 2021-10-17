a999d09dd56200b1102757fdb48672cf7f48a7f6
Deep Research <fisheve@mx.lakeforest.edu>
2021-10-17 04:59:50 -0500

    Logging Fetch

--- components/post-info.vue
+++ components/post-info.vue
@@ -63,6 +63,8 @@ export default Vue.extend({
   async fetch() {
     let article: any = await this.$content('/', { deep: true }).where({title: this.title}).fetch()
     this.article = article[0]
+
+    console.log(article)
   }
 })
 </script>
