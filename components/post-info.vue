d00c59f760cd3a5eade888360d981c315eaaf09c
Deep Research <fisheve@mx.lakeforest.edu>
2021-10-17 04:01:44 -0500

    Trying Deploy Without Category Section

--- components/post-info.vue
+++ components/post-info.vue
@@ -1,36 +1,23 @@
 <template>
   <div>
+    <!-- Article Title -->
     <h1 v-if="article.title">Article: {{ article.title }}</h1>
 
+    <!-- Link to Home -->
     <p><nuxt-link to="/">Home Page</nuxt-link></p>
     <br />
 
+    <!-- Metadata Section -->
     <div class="metadata">
 
+      <!-- Author -->
       <div v-if="article.author">Author: {{ article.author }}</div>
 
+      <!-- Published -->
       <div v-if="article.published">Published: {{ article.published }}</div>
 
+      <!-- Updated -->
       <div v-if="article.updated">Updated: {{ article.updated }}</div>
-
-      <div>
-
-        Category:
-         
-        <span
-          v-for="(item, index) in article.category"
-          :key="item"
-        >
-
-          <span class="circled-number">{{parseInt(index)}}</span>
-
-          <NuxtLink :to="'/'+item.toLowerCase()">{{ item }}</NuxtLink
-          
-          ><span
-            v-if="parseInt(index) < Object.keys(article.category).length"
-          >, </span>
-        </span>
-      </div>
     </div>
   </div>
 </template>
@@ -72,6 +59,6 @@ export default Vue.extend({
   }
 
   .metadata {
-    font-size: 120%
+    font-size: 120%;
   }
 </style>
