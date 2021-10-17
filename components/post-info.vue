c5525476d6422cdfd322a544d60252932019d0c0
Deep Research <fisheve@mx.lakeforest.edu>
2021-10-17 04:36:36 -0500

    Trying to Get Categories to Work

--- components/post-info.vue
+++ components/post-info.vue
@@ -1,14 +1,47 @@
 <template>
   <div>
+    <!-- Article Title -->
     <h1 v-if="article.title">Article: {{ article.title }}</h1>
 
+    <!-- Link to Home -->
     <p><nuxt-link to="/">Home Page</nuxt-link></p>
     <br />
 
-    <div style="font-size: 120%">
+    <!-- Metadata Section -->
+    <div class="metadata">
+
+      <!-- Author -->
       <div v-if="article.author">Author: {{ article.author }}</div>
+
+      <!-- Published -->
       <div v-if="article.published">Published: {{ article.published }}</div>
+
+      <!-- Updated -->
       <div v-if="article.updated">Updated: {{ article.updated }}</div>
+
+      <!-- Category -->
+      <div v-if="article.category">
+
+        <!-- Label -->
+        Category:
+         
+        <!-- Category Loop -->
+        <span
+          v-for="(item, index) in article.category"
+          :key="item"
+        >
+
+          <!-- Circled Number -->
+          <span class="circled-number">{{parseInt(index)}}</span>
+
+          <!-- Link to Category -->
+          <NuxtLink :to="'/'+item.toLowerCase()">{{ item }}</NuxtLink
+          
+          ><!-- Comma After Link --><span
+            v-if="parseInt(index) < Object.keys(article.category).length"
+          >, </span>
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -33,3 +66,23 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style scoped>
+  .circled-number {
+    background: #cccccc;
+    border-radius: 0.8em;
+    -moz-border-radius: 0.8em;
+    -webkit-border-radius: 0.8em;
+    color: #ffffff;
+    display: inline-block;
+    font-weight: bold;
+    line-height: 1.4em;
+    text-align: center;
+    width: 1.4em;
+    font-size: 75%;
+  }
+
+  .metadata {
+    font-size: 120%;
+  }
+</style>
