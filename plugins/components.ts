e57915e3bebc6c0489f319a353cf7974c22fa1b4
Deep Research <fisheve@mx.lakeforest.edu>
2021-10-24 02:52:15 -0500

    Update components.ts

--- plugins/components.ts
+++ plugins/components.ts
@@ -3,8 +3,8 @@
 import Vue from 'vue'
 import VRuntimeTemplate from "v-runtime-template"
 
-import song from '@/components/song.vue'
-import post from '@/components/post.vue'
+import song from '~/components/song.vue'
+import post from '~/components/post.vue'
     
 const components = { 
   VRuntimeTemplate,
