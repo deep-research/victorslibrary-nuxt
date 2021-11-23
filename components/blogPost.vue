9d1423eb158f0038b958f9c79896485a414a209a
Deep Research <fisheve@mx.lakeforest.edu>
2021-11-23 03:35:00 -0600

    Update blogPost.vue

--- components/blogPost.vue
+++ components/blogPost.vue
@@ -1,7 +1,49 @@
 <template>
   <client-only>
     <div>
-      {{ article }}
+      <!-- Article Title -->
+      <h1 v-if="article.title">Article: {{ article.title }}</h1>
+
+      <!-- Link to Home -->
+      <p><nuxt-link to="/">Home Page</nuxt-link></p>
+      <br />
+
+      <!-- Metadata Section -->
+      <div class="metadata">
+
+        <!-- Author -->
+        <div v-if="article.author">Author: {{ article.author }}</div>
+
+        <!-- Published -->
+        <div v-if="article.published">Published: {{ article.published }}</div>
+
+        <!-- Updated -->
+        <div v-if="article.updated">Updated: {{ article.updated }}</div>
+
+        <!-- Category -->
+        <div v-if="article.category">
+
+          <!-- Label -->
+          Category:
+          
+          <!-- Category Loop -->
+          <span
+            v-for="(item, index) in article.category"
+            :key="item.name"
+          >
+
+            <!-- Circled Number -->
+            <span class="circled-number">{{parseInt(index)}}</span>
+
+            <!-- Link to Category -->
+            <NuxtLink :to="item.url">{{ item.name }}</NuxtLink
+            
+            ><!-- Comma After Link --><span
+              v-if="parseInt(index) < Object.keys(article.category).length"
+            >, </span>
+          </span>
+        </div>
+      </div>
     </div>
   </client-only>
 </template>
